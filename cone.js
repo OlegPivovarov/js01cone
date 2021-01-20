cfa84f688c376b0aea2f807b9a99dfb583e3509c
OlegPivovarov <72058646+OlegPivovarov@users.noreply.github.com>
2021-01-20 20:48:26 +0900

    Add files via upload

--- cone.js
+++ cone.js
@@ -1,17 +1,34 @@
-// напишите функцию coneVolume(d,h) и используйте ее так,
-// так, чтобы функция solve() показывала объем ведра
-// либо сообщение "Недопустимые данные".
-
 function solve() {
-	// получаем данные в виде строк из полей ввода на странице
-	let d1 = document.getElementById("d1").value;
-	let d2 = document.getElementById("d2").value;
-	let h = document.getElementById("h").value;
+	let d1 = Number(document.getElementById("d1").value);
+	let d2 = Number(document.getElementById("d2").value);
+	let h = Number(document.getElementById("h").value);
 
-	alert(coneVolume(d1, h));
+	if(typeof(d1) === "number" && typeof(d2) === "number" && typeof(h) === "number" && d1>0 && d2>0 && h>0){
+		let h1 = 0;
+		let h2 = 0;
+		let qwe = 0;
+		let cV = 0;
+		if(d1 == d2){
+			cV = Math.PI*((d1/2)**2)*h;
+			alert(cV.toFixed(2));
+		}else{
+			if(d2>d1){
+				h1 = (h*d2)/(d2-d1);
+				h2 = h1-h;
+				qwe = coneVolume(d2,h1)-coneVolume(d1,h2);
+				alert(qwe.toFixed(2));
+			}else{
+				h1 = (h*d1)/(d1-d2);
+				h2 = h1-h;
+				qwe = coneVolume(d1,h1)-coneVolume(d2,h2);
+				alert(qwe.toFixed(2));
+			}
+		}
+	}else{
+		alert("недопустимые данные");	
+	}
 }
 
 function coneVolume(d, h){
-	// вычисление объема полного конуса
-	return d+1;
+	return (1/3)*Math.PI*((d/2)**2)*h;
 }
